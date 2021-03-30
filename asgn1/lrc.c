c7d8ff71b765e700a12775441b3493a7a843e5fe
Dp <dpan7@ucsc.edu>
2021-03-30 16:05:17 -0700

    Asking for user input

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
-#include <philos.h> /*Does this work? */
+#include "philos.h" /*Does this work? */
 
-main()
+int main()
 	
 {
+	int seed;
+	printf("Random seed: ");
+	scanf("%u", &seed);
+
+	int players;
+	printf("How many players? ");
+	scanf("%u", &players);
+}
+
+/*
 	//returns the position of the player on the left
 	//pos: Position of the current player
 	//players: The number of players in the game.
 
 	//The following two lines are from the asgn1.pdf lab1 documentation.
-	static inline uint8_t left (unit8_t pos, unit8_t players) {
-		return ((pos + players - 1) % players);
+static inline uint8_t left (unit8_t pos, unit8_t players) {
+	return ((pos + players - 1) % players);
 
 	//returns the position of the player on the right
         //The following two lines are from the asgn1.pdf lab1 documentation.
-	static inline unit8_t right(unit8_t pos, unit8_t players) {
-		return ((pos + 1) % players);
+static inline unit8_t right(unit8_t pos, unit8_t players) {
+	return ((pos + 1) % players);
 
         //The following two lines are from the asgn1.pdf lab1 documentation.
-	typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
+typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
 	faces die[] = { LEFT, RIGHT, CENTER, PASS, PASS, PASS };}
 
 	while () //While 1 >= # of players with $
+	*/
