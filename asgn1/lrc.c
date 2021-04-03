be25ab4b2fc3cd9fa114bb38f365d7653c732abe
Dp <dpan7@ucsc.edu>
2021-04-02 18:02:17 -0700

    added conditions for false inputs, converted some int to unsigned int, and fixed the counter for centerpot

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -7,7 +7,7 @@ typedef struct {
 	char name[50];
 	int cash;
 	int pos;
-		}variables ;
+		}variables;
         //returns the position of the player on the left
         //pos: Position of the current player
         //players: The number of players in the game.
@@ -21,8 +21,7 @@ static inline uint8_t right(uint8_t pos, uint8_t players) {
         return ((pos + 1) % players);
 	}
         //The following two lines are from the asgn1.pdf lab1 documentation
-
-void money(int a, int players , variables ppl[14], int pot){ //give money to l/r/pot/pass
+int money(int a, unsigned int players , variables ppl[14]){ //give money to l/r/pot/pass
 	typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
 		faces die[] = { LEFT, RIGHT, CENTER, PASS, PASS, PASS };
 	int rando = rand() % 6;
@@ -30,26 +29,30 @@ void money(int a, int players , variables ppl[14], int pot){ //give money to l/r
 		uint8_t leftt = left(a, players);//Get person on left position
 		ppl[leftt].cash = ppl[leftt].cash + 1; //Give a dollar
 		ppl[a].cash = ppl[a].cash -1;//Lose a dollar
-		printf("gives $1 to %s", ppl[leftt].name);
+		printf(" gives $1 to %s", ppl[leftt].name);
+		return 0;	
 		}
 	else if (die[rando] == RIGHT){
 		uint8_t rightt = right(a, players);
 		ppl[rightt].cash = ppl[rightt].cash + 1;
 		ppl[a].cash = ppl[a].cash - 1;
-		printf("gives $1 to %s", ppl[rightt].name);
+		printf(" gives $1 to %s", ppl[rightt].name);
+		return 0;
 		}
+
+
 	else if (die[rando] == CENTER){
-		pot = pot + 1;
 		ppl[a].cash = ppl[a].cash -1;
-		printf("puts $1 in the pot\n");
+		printf(" puts $1 in the pot");
+		return 1;
 		}
 	else{
-		printf("gets a pass");
+		printf(" gets a pass");
+		return 0;
 		}
 }
-
-int check(int players, variables ppl[14]){ //Check if the game should keep going
-	int i = 0;
+int check(unsigned int players, variables ppl[14]){ //Check if the game should keep going
+	unsigned int i = 0;
 	int cashtest = 0;
 	for (i=0; i<players; i++){
 		if (ppl[i].cash > 0){ //if only one person has cash then cashtest =1
@@ -62,7 +65,6 @@ int check(int players, variables ppl[14]){ //Check if the game should keep going
 	else{
 		return 0;
 		}
-	
 	}	
 int main(){
 	//Ask for the random seed and the amount of players
@@ -70,23 +72,27 @@ int main(){
 	printf("Random seed: ");
 	scanf("%u", &seed);
 	srandom(seed);
-	int players;
+	unsigned int players;
 	printf("How many players? ");
-	scanf("%u", &players);
+	if(scanf("%u", &players)==EOF||players > 14 || players < 1 ){	
+		printf("Number of players must be from 1 to 14.\n");
+		return 0;
+		}
 	int pot = 0;
-	
 	variables ppl[players];
-	int i;
+	unsigned int i;
 	for (i =0; i<players; i++){ //Create an array of data		
 		strcpy(ppl[i].name, philosophers[i]);
 		ppl[i].cash = 3;
 		ppl[i].pos = i;
-		}		
+		}	
 	
-
-
+	if (players ==1){
+                printf("%s wins the $%d pot with $%d left in the bank!\n", ppl[0].name,pot, ppl[0].cash);
+		return 0;
+		}
 	while(check(players, ppl) ==0) {
-		int a;
+		unsigned int a;
 		for (a=0; a<players; a++){
 			if (ppl[a].cash == 0){	
 				continue;
@@ -94,33 +100,33 @@ int main(){
 			else if (ppl[a].cash ==2){
 				//roll 2 dice
 				printf("%s rolls...", ppl[a].name); //Name rolls... 
-				money(a,players, ppl,pot);
-				money(a,players, ppl,pot);
+				pot = pot + money(a,players, ppl);
+				pot = pot + money(a,players, ppl);
 				printf("\n");
 				}
 			else if (ppl[a].cash == 1){ 
 				printf("%s rolls...", ppl[a].name); //Name rolls... 
-                                money(a,players, ppl,pot);
+                                pot = pot + money(a,players, ppl);
 				printf("\n");
+				
 				}
 			else{
                                 printf("%s rolls...", ppl[a].name); //Name rolls... 
-                                money(a,players, ppl,pot);
-                                money(a,players, ppl,pot);
-				money(a,players, ppl,pot);
+                                pot = pot + money(a,players, ppl);
+                                pot = pot + money(a,players, ppl);
+				pot = pot + money(a,players, ppl);
 				printf("\n");
 				}
+
 			if (check(players,ppl) ==1){
-				int j;
+				unsigned int j;
 				for (j=0; j<players; j++){
 					if (ppl[j].cash >= 1){		
-						printf("%s wins %d", ppl[j].name, ppl[j].cash);
+						printf("%s wins the $%d pot with $%d left in the bank!\n", ppl[j].name,pot, ppl[j].cash);
 						return 0 ;
 						}
 					}
 				}
 			}
-			
 		}
-	
 }
