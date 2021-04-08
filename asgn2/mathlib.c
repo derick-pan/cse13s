2cff6b6e04ed44762f61e35a5bccfac37f42d5ee
Dp <dpan7@ucsc.edu>
2021-04-07 17:26:30 -0700

    Pushing base files

---
+++ asgn2/mathlib.c
@@ -0,0 +1,24 @@
+#include "mathlib.h"
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(void) {
+	
+	
+	
+}
+
+
+double arcSin() {
+
+
+double arcCos() {
+
+
+double arcTan() {
+
+
+double Log() {
+
