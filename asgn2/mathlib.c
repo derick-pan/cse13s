0a241b4004ba04c88f85dc790812129e78d943e6
Dp <dpan7@ucsc.edu>
2021-04-07 17:54:05 -0700

    Completed the factorial and paower functions, Starting arcsin()

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -2,16 +2,42 @@
 #include <stdio.h>
 #include <math.h>
 
+double ffactorial(double base){//Function to simplify Factorials
+	double ans = 1;
+	double i = 1;
+	for(i=1; i <= base; i++){
+		ans = ans*i
+	}
+	return ans;
+}	
 
-
-int main(void) {
-	
-	
-	
+double paower(double base, double top){//Function to simplify to powers
+	double ans =1 ;
+	double i =1;
+	for(i=1; i <= top; i++){
+		ans = ans * base;
+	}
+	return ans;
 }
-
-
+int main(void) {
+}
 double arcSin() {
+	double x = -1
+
+	for(x= -1; x < 1; x + 0.1){//Increment X from -1 to 1, step 0.1
+		
+		double k = 0;
+		for(k = 0; 0; k++){ //Taylor Expansion
+			double tk = 2 * k;
+			double factk = ffactorial(k);
+			double facttk = ffactorial(tk);	
+
+			
+
+					
+
+
+
 
 
 double arcCos() {
