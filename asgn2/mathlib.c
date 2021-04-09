8db214db8582906d2be0ccf07240266e2ca62bb5
Dp <dpan7@ucsc.edu>
2021-04-09 15:39:56 -0700

    editing mathlib.c for mathlib-test.c

--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,8 +1,7 @@
 #include "mathlib.h"
-
+#include <mathlib-test.c>
 #include <math.h>
 #include <stdio.h>
-
 double ffactorial(double base) { //Function to simplify Factorials
     double ans = 1;
     double i = 1;
@@ -20,41 +19,33 @@ double paower(double base, double top) { //Function to simplify to powers
     }
     return ans;
 }
-int main(int argc, char **argv[]) {
 
-	int choice;
+double epsilonn = paower(10, -10);
 
-	while (( choice = getopt(argc,argv, "ascti" )) != -1) {
-
-		switch (choice) {
-			case 'a':
-			
-			case 's':
-
-			case 'c':
-
-			case 't':
-			
-			case 'i':	
-			
-			default:
-				fprintf(stderr, "print the program usage?");
-		}
-	}
-}
-double arcSin() {
-    double x = -1
-
-               for (x = -1; x < 1; x + 0.1) { //Increment X from -1 to 1, step 0.1
-
-        double k = 0;
+double arcSin(double x) {
+    //double x = -1;
+    //for (x = -1; x < 1; x + 0.1) { //Increment X from -1 to 1, step 0.1
+	//double Finalans = x;
+        //double k = 0;
+	double Finalans = x;
+	double k = 0;	
         for (k = 0; 0; k++) { //Taylor Expansion
             double tk = 2 * k;
             double factk = ffactorial(k);
             double facttk = ffactorial(tk);
+	    double numerator = facttk * paower(x, tk+1);
+	    double denominator = paower(2, tk) * paower(factk,2) *(tk+1);
+	    double tempans = numerator/denominator;
+	    Finalans = tempans + Finalans;
+	    if (x-Finalans <= epsilonn) {
+		    return Finalans;
+        }
+    return 1;
+}
 
-            double arcCos() {
-
-                double arcTan() {
-
-                    double Log() {
+double arcCos() {
+}
+double arcTan() {
+}
+double Log() {
+}
