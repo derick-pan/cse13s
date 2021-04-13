309dde814e1ae67e3edf1cde03fffa179698bc0f
Dp <dpan7@ucsc.edu>
2021-04-13 15:38:14 -0700

    adding comments

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -1,94 +1,93 @@
 //Derick Pan
 //dpan7
 //Asgn2 2021
+#include "mathlib.h"
+
+#include <math.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "mathlib.h"
-#include <math.h>
 //This is the test harness for the mathlib implementations.
 //The following are 5 flags, a for all tests, s= sin, c=cos, t=tan, l=log
 //The flags are set to 0, and if they change to 1, it means that the user selected that choice
 int main(int argc, char *argv[]) {
-	int flaga = 0;
-	int flags = 0;
-	int flagc = 0;
-	int flagt = 0;
-	int flagl = 0;
+    int flaga = 0;
+    int flags = 0;
+    int flagc = 0;
+    int flagt = 0;
+    int flagl = 0;
     double libr;
     int choice;
-    
-    while ((choice = getopt(argc, argv, "asctl")) != -1) {//Loop that goes over the user inputs
+
+    while ((choice = getopt(argc, argv, "asctl")) != -1) { //Loop that goes over the user inputs
         switch (choice) {
-        case 'a':
-		flaga = 1;
-		break;	
-        case 's':
-		flags = 1;
-		   break;
-	case 'c':
-		flagc = 1;
-		   break;
-	case 't':
-		flagt = 1;
-		   break;
-	case 'l':
-		flagl = 1;
-	   break;
-	case '?'://If unrecognized input,print usage, then break
-	    printf("Program usage: ./mathlib - [asctl]\n");
-	    printf("   -a   Runs all tests (arcsin, arccos, arctan, log\n");
-	    printf("   -s   Runs arcsin tests\n");
-	    printf("   -c   Runs arccos tests\n");
-	    printf("   -t   Runs arctan tests\n");
-	    fprintf(stderr,"   -l   Runs log tests\n");
-	    break;
-	case ':'://If unrecognized input then break
-	    break;
+        case 'a': flaga = 1; break;
+        case 's': flags = 1; break;
+        case 'c': flagc = 1; break;
+        case 't': flagt = 1; break;
+        case 'l': flagl = 1; break;
+        case '?': //If unrecognized input,print usage, then break
+            printf("Program usage: ./mathlib - [asctl]\n");
+            printf("   -a   Runs all tests (arcsin, arccos, arctan, log\n");
+            printf("   -s   Runs arcsin tests\n");
+            printf("   -c   Runs arccos tests\n");
+            printf("   -t   Runs arctan tests\n");
+            fprintf(stderr, "   -l   Runs log tests\n");
+            break;
+        case ':': //If unrecognized input then break
+            break;
         }
     }
     //If input is invalid, then print program usage
-    if (argv[optind] == NULL || argv[optind +1] == NULL) {
-	    printf("Program usage: ./mathlib - [asctl]\n");
-	    printf("   -a   Runs all tests (arcsin, arccos, arctan, log\n");
-	    printf("   -s   Runs arcsin tests\n");
-	    printf("   -c   Runs arccos tests\n");
-	    printf("   -t   Runs arctan tests\n");
-	    fprintf(stderr,"   -l   Runs log tests\n");
+    if (argv[optind] == NULL || argv[optind + 1] == NULL) {
+        printf("Program usage: ./mathlib - [asctl]\n");
+        printf("   -a   Runs all tests (arcsin, arccos, arctan, log\n");
+        printf("   -s   Runs arcsin tests\n");
+        printf("   -c   Runs arccos tests\n");
+        printf("   -t   Runs arctan tests\n");
+        fprintf(stderr, "   -l   Runs log tests\n");
     }
-  double x;//This x is the value to pass onto the functions
+    double x; //This x is the value to pass onto the functions
 
-  if (flaga== 1 || flags ==1) { //If all test's flag or arcsin flag is ==1, then print values from -1 to 1, w/ step 0.1
-                printf("  x\t       arcSin\t\tLibrary\t       Difference\n" ); 
-                printf("  -\t       ------\t\t-------\t       ----------\n" ); 
-            for (x = -1; x < 1; x += 0.100000000) {
-		libr = asin(x);
-                printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n",x ,arcSin(x) , libr, arcSin(x)-libr);
-            }
-	 }
- if(flaga==1 || flagc == 1) { //If all test's flag or arccos flag is ==1, then print values from -1 to 1, w/ step 0.1
-                printf("  x\t       arcCos\t\tLibrary\t       Difference\n" ); 
-                printf("  -\t       ------\t\t-------\t       ----------\n" ); 
-	    for (x = -1; x <1; x += 0.1) {
-		libr = acos(x);
-                printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n",x ,arcCos(x) , libr, arcCos(x)-libr);
-            }
- }
- if(flaga==1 || flagt == 1) {//If all test's flag or acrcos flag is ==1, then print values from 1 to 10, w/ step 0.1
-                printf("  x\t       arcTan\t\tLibrary\t       Difference\n" ); 
-                printf("  -\t       ------\t\t-------\t       ----------\n" ); 
-	    for (x = 1; x <10; x += 0.1) {
-		libr = atan(x);
-                printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n",x ,arcTan(x) , libr, arcTan(x)-libr);
-            }
- }
- if(flaga==1 || flagl == 1) { //If all test's flag or log flag is ==1, then print values from 1 to 10, w/ step 0.1
-                printf("  x\t       Log   \t\tLibrary\t       Difference\n" ); 
-                printf("  -\t       ------\t\t-------\t       ----------\n" ); 
-	    for (x = 1; x <10; x += 0.1) {
-		libr = log(x);
-                printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n",x ,Log(x) , libr, Log(x)-libr);
-            }
- }
+    if (flaga == 1
+        || flags
+               == 1) { //If all test's flag or arcsin flag is ==1, then print values from -1 to 1, w/ step 0.1
+        printf("  x\t       arcSin\t\tLibrary\t       Difference\n");
+        printf("  -\t       ------\t\t-------\t       ----------\n");
+        for (x = -1; x < 1; x += 0.100000000) {
+            libr = asin(x);
+            printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", x, arcSin(x), libr, arcSin(x) - libr);
+        }
+    }
+    if (flaga == 1
+        || flagc
+               == 1) { //If all test's flag or arccos flag is ==1, then print values from -1 to 1, w/ step 0.1
+        printf("  x\t       arcCos\t\tLibrary\t       Difference\n");
+        printf("  -\t       ------\t\t-------\t       ----------\n");
+        for (x = -1; x < 1; x += 0.1) {
+            libr = acos(x);
+            printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", x, arcCos(x), libr, arcCos(x) - libr);
+        }
+    }
+    if (flaga == 1
+        || flagt
+               == 1) { //If all test's flag or acrcos flag is ==1, then print values from 1 to 10, w/ step 0.1
+        printf("  x\t       arcTan\t\tLibrary\t       Difference\n");
+        printf("  -\t       ------\t\t-------\t       ----------\n");
+        for (x = 1; x < 10; x += 0.1) {
+            libr = atan(x);
+            printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", x, arcTan(x), libr, arcTan(x) - libr);
+        }
+    }
+    if (flaga == 1
+        || flagl
+               == 1) { //If all test's flag or log flag is ==1, then print values from 1 to 10, w/ step 0.1
+        printf("  x\t       Log   \t\tLibrary\t       Difference\n");
+        printf("  -\t       ------\t\t-------\t       ----------\n");
+        for (x = 1; x < 10; x += 0.1) {
+            libr = log(x);
+            printf(" %7.4lf % 16.8lf % 16.8lf % 16.10lf\n", x, Log(x), libr, Log(x) - libr);
+        }
+    }
 
     return 0;
 }
--- asgn2/mathlib.c
+++ asgn2/mathlib.c
@@ -1,87 +1,86 @@
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
+//Derick Pan
+//dpan7
+//Asgn2 2021
 #include "mathlib.h"
-//mathlib.c//
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 double epsilonn = 0.000000001;
 
-
-double abss(double val) {
-	if (val < 0) {
-	    val = val * -1;
-	    return val;
-	}
-	return val;
+double abss(double val) { //Returns the absolute value
+    if (val < 0) {
+        val = val * -1;
+        return val;
+    }
+    return val;
 }
 double Exp(double x) { //Exp() function is from Professor Long
-	double term = 1, sum = 1;
-	for (int k = 1; abss(term) > epsilonn; k += 1) {
-		term *= x/k;
-		sum += term;
-	}
-	return sum;
+    double term = 1, sum = 1;
+    for (int k = 1; abss(term) > epsilonn; k += 1) {
+        term *= x / k;
+        sum += term;
+    }
+    return sum;
 }
-double arcSin(double x) {
-    double Finalans = x;
-    double k = 0;
-    double top = 1;
-    double bot = 2;
-    double ex = x*x*x;
-    double exbot = 3;
-    double leftpass = 1;
-    double testans = 0;
+double arcSin(double x) { // Calculates arcSin(x) through a simplified Maclaurin Series
+    double Finalans = x; //The current iteration's answer.
+    double k = 0; //Counter for # of iterations. This isn't printed but is nice to have
+    double top = 1; //Top is the lefthand side numerator             EXPLAINED IN DESIGN.pdf
+    double bot = 2; //Bot is the lefthand side denominator           EXPLAINED IN DESIGN.pdf
+    double ex = x * x * x; //exp is the right hand side's numerator's simplified power
+    double exbot = 3; //exbot is the right hand side denominator.	  EXPLAINED IN DESIGN.pdf
+    double leftpass = 1; //Leftpass is the current iteration's left side  EXPLAINED IN DESIGN.pdf
+    double testans = 0; //testans is the previous iteration's answer     EXPLAINED IN DESIGN.pdf
+
     for (;;) { //Taylor Expansion
-	leftpass = leftpass * (top/bot);
-	Finalans = Finalans+(leftpass * (ex/exbot));
-	k = k + 1; 
-	ex = ex*x*x;
-	exbot = exbot+2;
-	bot = bot +2;
-	top = top +2;
-        if (abss(abss(Finalans) - abss(testans)) < epsilonn) {
+        leftpass = leftpass * (top / bot); //Multiply the previous left sides by top/bot
+        Finalans = Finalans + (leftpass * (ex / exbot)); //Get the current answer
+        k = k + 1; //Counter +=1
+        ex = ex * x * x; //After each iteration, multiply x*x*ex
+        exbot = exbot + 2; //Right side denominator +2 for the pattern.
+        bot = bot + 2; //Left side denominator +2 for the pattern.
+        top = top + 2; //Left side Numerator +2 for the pattern
+        if (abss(abss(Finalans) - abss(testans))
+            < epsilonn) { //test if the difference from current and last ieration < epsilon
             return Finalans;
-	}
-    testans = Finalans;
+        }
+        testans = Finalans;
     }
     return 1;
 }
 
-double arcCos(double x) {
-	return ((M_PI/2) - arcSin(x));
+double arcCos(double x) { //arcCos found by subtracting arcSin(x) from Pi/2.
+    return ((M_PI / 2) - arcSin(x));
     return 0;
 }
-double arcTan(double x) {
-	
-	double below = (x*x) + 1;
-	double test = below;
-	double finalans = below;
-	for(;;) {
-	    test = finalans;
-	    finalans= ((test + below/test)/2);//Newtons method
-	    if (abss(abss(finalans)-abss(test)) < 0.00000000001) {
-		return (arcSin(x/finalans));
-   	    }
-        }	    
-	return 0;
+double arcTan(double x) { //Arctan found by arcsin(x/sqrt(x*x+1))
+
+    double below = (x * x) + 1; //Below is the value getting passed to arcsin's denominator
+    double test = below; //Test is the answer from previous iteration for Square rooting
+    double finalans = below; //Finalans is the Current Answer for square rooting
+    for (;;) {
+        test = finalans; //Set the previous value to test.
+        finalans
+            = ((test + below / test) / 2); //Newtons method   //Find the current iterations number
+        if (abss(abss(finalans) - abss(test))
+            < epsilonn) { //If the difference from finalans and test is less than epsilon,
+            //Then that is close to the square root.
+            return (arcSin(x / finalans)); //Return arcsin(x/finalans)
+        }
+    }
+    return 0;
 }
 double Log(double x) {
-	double pass = 1;
-	double epow;
-	for(;;) {
-	    	epow = Exp(pass);
-		pass = pass + (abss(x) - epow)/epow;
-	    	if (abss(abss(x) - abss(epow)) < epsilonn){
-			return pass;
-		}
-	}
-
-
-
+    double pass = 1;
+    double epow; //Declare the e^ xk. This is declared because it's used TWICE in an iteration.
+    for (;;) {
+        epow = Exp(pass); //Find epow once, and it will be used TWICE in the following.
+        pass = pass + (abss(x) - epow) / epow; //Pass is the current ans
+        if (abss(abss(x) - abss(epow)) < epsilonn) { //Test if difference is less than epsilon
+            return pass; //Return the value.
+        }
+    }
 
     return 0;
-    
-
-
 }
