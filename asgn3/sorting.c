1abf191f355f475ef21c85bcdb8d06faed356068
Dp <dpan7@ucsc.edu>
2021-04-18 14:01:55 -0700

    Working on test harnes

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -9,25 +9,50 @@
 #include "queue.h"
 
 int main() {
-	flaga = 0;
-	flagb = 0;
-	flags = 0;
-	flagq = 0;
+	flaga = 0;//All sorting Algorithms
+	flagb = 0;//Bubble sort
+	flags = 0;//Shell Sort
+	flagq = 0;//Stack
+	flagQ = 0;//Queue
+		flagr = 13371453;//seed
+	flagn = 100;//Size
+	flagp = 100;//Elements to print
 	while((choice = getopt(argc,argv, "absqQr:n:p:")) != -1) {
 
 		switch(choice) {
 		case 'a':
+			flaga = 1;
 		case 'b':
 			flagb = 1;
 		case 's':
-			flag
+			flags = 1;
 		case 'q':
+			flagq = 1;
 		case 'Q':
+			flagQ = 1;
 		case 'r':
+			if (optarg != NULL && optarg >0) {
+				srandom(optarg);
+				break;
+			}
+			srandom(13371453);
 		case 'n':
+			if (optarg != NULL && optarg >0) {
+				flagn = optarg;
+				break;
+			}
 		case 'p':
+			if (optarg != NULL && optarg >0) {
+				flagp = optarg;
+				break;
+			}
+		}
+	}
+	if(flagb ==1){
+		float x[8] = {123,16,89,632,1,97,45,29};
+		bubble_sort(sizeof(x), x)}
+}
 		
-
 
 
 
