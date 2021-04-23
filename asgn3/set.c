64e267eace25497c56a2c85cae0ee30692bdda0c
Dp <dpan7@ucsc.edu>
2021-04-22 19:57:09 -0700

    sets

--- asgn3/set.c
+++ asgn3/set.c
@@ -1,9 +1,10 @@
 #include "set.h"
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+ 
+
 typedef uint32_t Set;
 #define SET_CAPACITY 32 // 32 bits means 32 items max.
 
@@ -11,18 +12,22 @@ Set set_empty(void) { //return an empty set
     uint32_t s = 0;
     return s;
 }
+
+
 bool set_member(Set s, int x) { //True if x in set
     uint32_t y = (uint32_t)(int) x;
-    //	printf("\nset: %u x: %u\n", s, y);
-
     return ((s & y) == y);
 }
+
+
 Set set_insert(Set s, int x) {
     s = (s | x);
-    //	printf("Set_insert: %u\n", s);
-    //	printf("Set after insert:  %u \n",s);
     return s;
 }
+
+
+// I don't need these //
+
 Set set_remove(Set s, int x);
 
 Set set_union(Set s, Set t);
