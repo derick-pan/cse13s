8d7c83c5e82d4c591311f0135634f2443b33910d
Dp <dpan7@ucsc.edu>
2021-04-26 19:38:18 -0700

    starting to make graph:

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -22,7 +22,13 @@ Graph *graph_create(uint32_t vertices, bool undirected) {
 		G->undirected =	undirected;
 		G->visited[VERTICES] = false;
 	        G->matrix[VERTICES][VERTICES] = 0;	
-		int weight =0;
+
+		for (int i =0; i<=vertices; i++) {
+			G->visted[VERTICES] = false;		
+			for (int j=0; j<=vertices; j++) {
+				G->matrix[i][j] =0;	
+			}	
+		}
 	}
 	return G;
 }
@@ -43,7 +49,17 @@ uint32_t graph_vertices(Graph *G) {
 }
 bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
 
+	if (g->undirected && g->matrix[j][i] != NULL ) {
+		g->matrix[j][i] = k;
+	}
 
+	if(g->matrix[i][j] != NULL){
+		g->matrix[i][j] ==k;
+	}
+
+
+	//add edge
+	//weight k from j to i
 
 
 
