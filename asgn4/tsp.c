fa39949040ad5bc6794c00ec74c565f93f9cb7f3
Dp <dpan7@ucsc.edu>
2021-04-28 14:53:51 -0700

    working on recursion

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -14,32 +14,43 @@
 #include <unistd.h>
 
 char *cities;
-
+uint32_t len = 100;
 void dfs(Graph *G, uint32_t v, Path *c, Path *s) {
 
     graph_mark_visited(G, v);
     uint32_t x;
 //If is a hamiltonian path shorter than anything ever seen before
-    if (path_length(c) < path_length(s)){
-        printf("here?");
-        printf("Before C:%u  ", path_vertices(c));
-        printf("Before S:%u  \n", path_vertices(s));
+
+//If current path is hamiltonian
+
+    if (path_length(c) < len || path_length(c) < path_length(s)){
+        //printf("here?");
+        //printf("Before C:%u  ", path_vertices(c));
+        //printf("Before S:%u  \n", path_vertices(s));
         path_copy(s, c);
-        printf("After C: %u  ", path_vertices(c));
-        printf("After S: %u  \n", path_vertices(s));
+        //printf("After C: %u  ", path_vertices(c));
+        //printf("After S: %u  \n", path_vertices(s));
     }
 
     for (uint32_t w = 0; w < graph_vertices(G); w++) { //For all edges
 
         if (graph_has_edge(G, v, w) && !graph_visited(G, w)) { //Only edges and not visited
             path_push_vertex(c, w, G); //Push it onto the stack
+            //path_push_vertex(s, w, G); //Push it onto the stack
+
             dfs(G, w, c, s); //test it recursively
+            if (path_length(c)< len){
+                len = path_length(c);
+            }
             path_pop_vertex(c, &x, G); //Pop the stack after testing all of dfs
+            //path_pop_vertex(s, &x, G); //Pop the stack after testing all of dfs
 
         } //If all visited then it stops
         //dfs(G, v, c, s);
+
     }
     graph_mark_unvisited(G, v);
+    printf("Path Length S: %u\n Path Length C: %u \n",path_length(s),path_length(c));
 }
 
 int main(int argc, char *argv[]) {
@@ -53,14 +64,10 @@ int main(int argc, char *argv[]) {
 
         case 'v': //Verbose printing
             break;
-
-
         case 'u': undirectedflag = true; break;
-
         case 'i': snprintf(file, 20,"%s", optarg); break; //The file
         case 'o': flago=true; break; //stdout
         }
-
     }
     char buffer[1024];
     char* token;
@@ -89,13 +96,18 @@ int main(int argc, char *argv[]) {
                 token = strtok(NULL, " ");
 
             }
-            printf("%d, %d, %d\n", temp[0], temp[1], temp[2]);
+            //printf("%d, %d, %d\n", temp[0], temp[1], temp[2]);
             graph_add_edge(G, temp[0], temp[1], temp[2]);
         }
-
     }
-    fclose(read);
     graph_print(G);
+    Path *c = path_create(); //For current path
+    Path *s = path_create(); //For Previous Path
+
+    dfs(G, 0, c, s);
+    printf("Path Length: %u\n",path_length(c));
+    fclose(read);
+
 
     graph_delete(&G);
 }
