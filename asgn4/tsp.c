106b6b2eb8567d09ae433b078fedc8bfcca77cfc
Dp <dpan7@ucsc.edu>
2021-04-30 05:14:51 -0700

    Recursion is printing El-Cajon -> La-Mesa -> Chul  -> Lakeside -> Jamul -> Campo -> La-Jolla -> Santee ->  Tijuana -> San-Ysidro

--- asgn4/tsp.c
+++ asgn4/tsp.c
@@ -1,6 +1,6 @@
-//Derick Pan
-//dpan7
-//Asgn4
+// Derick Pan
+// dpan7
+// Asgn4
 #include "graph.h"
 #include "path.h"
 #include "stack.h"
@@ -11,175 +11,161 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-int recuse = 0;
+char usage[1000] = "SYNOPSIS\n\
+  Traveling Salesman Problem using DFS.\n\n\
+USAGE\n\
+  ./tsp [-u] [-v] [-h] [-i infile] [-o outfile]\n\n\
+OPTIONS\n\
+  -u             Use undirected graph.\n\
+  -v             Enable verbose printing.\n\
+  -h             Program usage and help.\n\
+  -i infile      Input containing graph (default: stdin)\n\
+  -o outfile     Output of computed path (default: stdout)\n";
+
+int64_t recuse = 0;
 int flagham;
-
 void dfs(Graph *G, uint32_t v, Path *c, Path *s, FILE *outfile, char *cities[]) {
+    printf("rec %ld\n", recuse);
     recuse += 1;
+    flagham = 0;
     graph_mark_visited(G, v);
-    //path_print(c, outfile, cities);
-
-    //printf("got into dfs here\n");
-//   Hamiltonian if your path goes through all the points and gets back
-//   to beginning, that is Hamiltonian
-//Shortest hamiltonian is the shortest one that does so
-    /*
-    if (v==0 && path_vertices(c)){ //If this is my first vertex, add 1 to vertex
-        printf("ONCE \n");
-        path_push_vertex(c, START_VERTEX,G);
-        path_copy(s, c);
-    }*/
-
-
-    /* Loop to test if ALL have been visited */
-    flagham =0;
-    for (uint32_t q = v; q < graph_vertices(G); q++){
-        if (!graph_visited(G,q)){
-            flagham = 1; //If flagham ==1 then something wasn't visited
+    // path_print(s, outfile, cities);
+    for (uint32_t q = 0; q < graph_vertices(G); q++) {
+        if (!graph_visited(G, q)) {
+            flagham = 1; // If flagham ==1 then SOME POINT is not visited
             break;
         }
     }
 
- /* if   All visted &&        all vertices but current   */
+    //Hamiltonian = Go through all the points NO MORE THAN ONCE.
 
- // !(If all is visited and last one has edge to origin)
- //     All Not visited or last one doesn't have edge to origin
+    if (flagham == 0) {
 
-    if (flagham ==0 && (graph_has_edge(G, v,START_VERTEX) )){
-        printf("+1 hamiltonian Path\n");
-
-        if (path_length(s)==0 || (path_length(c) < path_length(s))){
-            printf("am i ever here??, Recursion # : %u \n", recuse);
-            path_push_vertex(c, v,G); //Add in the final point
+        if (path_vertices(s) == 0 && path_length(s)==0 ) { //This if statement seems fine
+            printf("I'm a solo\n");
+            //path_push_vertex(c, START_VERTEX, G); // Add in the final point
             path_copy(s, c);
             path_print(s, outfile, cities);
-            return;
+
         }
-        else{return;} //Not the shortest one
-    }
-    else if (flagham ==0){
+        if ((path_length(c) < path_length(s)) && (graph_has_edge(G,v, 0))) {
+
+            //path_push_vertex(c, START_VERTEX, G); // Add in the final point
+            path_copy(s, c);
+            fprintf(outfile, "am i the corrupted one?");
+            path_print(s, outfile, cities);
+            //return;
+        }
+        graph_mark_unvisited(G, v);
         return;
     }
-    //If i visited all vertices buy next one isn't orgin What do i do?
-    // IF I visited all vertices && my stack top is origin then???
 
     uint32_t x;
-    for (uint32_t w = 0; w <= graph_vertices(G); w++) { //For all edges
-        if (graph_has_edge(G, v, w) && !graph_visited(G, w)) { //Only edges and not visited
-            path_push_vertex(c, w, G);  //Push it onto the stack
-            dfs(G, w, c, s, outfile, cities);            //test it recursively
-            path_pop_vertex(c, &x, G);  //Pop the stack after testing all of dfs
+    for (uint32_t w = 0; w < graph_vertices(G); w += 1) { // For all edges
+        if ((!graph_visited(G, w)) && (graph_has_edge(G,v, w))) { // Only edges and not visited
+            path_push_vertex(c, w, G); // Push it onto the stack
+            dfs(G, w, c, s, outfile, cities); // test it recursively
+            path_pop_vertex(c, &x, G); // Pop the stack after testing all of dfs
         }
     }
     graph_mark_unvisited(G, v);
 }
 
+/*
+Mines:
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul ->
+        Chula-Vista -> Lakeside -> El-Cajon
+
+Correct one:
+        s->items[i] =2
+s->items[i] =0
+El-Cajon -> La-Mesa -> Jamul -> Campo -> La-Jolla -> Tijuana -> San-Ysidro ->
+Santee -> Chula-Vista -> Lakeside -> El-Cajon
+
+
+         s->items[i] =1         s->items[i] =3
+s->items[i] =0      s->items[i] =2              s->items[i] =4
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Mesa -> La-Jolla -> Tijuana -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Mesa -> La-Jolla -> Tijuana -> El-Cajon
 
 
 
-////////###################################################################################/
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Jolla -> Jamul -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Jolla -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> Campo -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> San-Ysidro -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> Chula-Vista -> Lakeside -> El-Cajon
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> Chula-Vista -> Lakeside -> El-Cajon
 
+
+
+
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Mesa -> La-Jolla -> Tijuana -> El-Cajon
+
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> La-Mesa -> La-Jolla -> Tijuana -> El-Cajon
+
+
+El-Cajon -> Santee -> Lakeside -> Chula-Vista -> San-Ysidro -> Campo -> Jamul -> Chula-Vista -> Lakeside -> El-Cajon
+*/
 int main(int argc, char *argv[]) {
     int choice;
-    //bool flago= false;
-    //bool undirectedflag= true;
-    //char *cities[] = { "El-C", "santee", "lakeside", "chula", "san", "camp", "jam", "lames",
-    //"lajol", "tijua" };
+    bool undir = false;
     char file[20];
+    char outf[50];
     while ((choice = getopt(argc, argv, "hv:ui:o:")) != -1) {
         switch (choice) {
-        case 'h': break; //Print helps
-        case 'v': //Verbose printing
-            break;
-        case 'u': //undirectedflag = true; break;
-        case 'i': snprintf(file, 20, "%s", optarg); break; //The file
-        case 'o': break; //flago=true; break; //stdout
+        case 'h': fprintf(stderr, "%s", usage); break; // Print helps
+        case 'v': break; // Verbose printing
+        case 'u': undir = true; break;
+        case 'i': snprintf(file, 20, "%s", optarg); break; // The file
+        case 'o': snprintf(outf, 50, "%s", optarg); break;
+        case '?': fprintf(stderr, "%s", usage); break;
         }
     }
     char buffer[1024];
     char *token;
-
     uint32_t temp[] = { 0, 0, 0 };
-
-    //printf("%s", file);
-
     FILE *read = fopen(file, "r");
     fgets(buffer, 1023, read);
-    int amcities = atoi(buffer);
-    Graph *G = graph_create(amcities, false);
-    char *cities[amcities];
+    uint32_t amcities = atoi(buffer);
 
-    for (int i = 0; fgets(buffer, 1023, read) != NULL; i++) {
-	buffer[strcspn(buffer, "\n")] = 0;
+    Graph *G = graph_create(amcities, undir);
+    char *cities[amcities - 1];
+    for (uint32_t i = 0; fgets(buffer, 1023, read); i++) {
+        buffer[strcspn(buffer, "\n")] = 0;
         if ((i) < amcities) {
-            //strcpy(cities[i],buffer);
             cities[i] = strndup(buffer, 1023);
-            printf("hi %s\n", cities[i]);
         }
         if (i < amcities) {
             continue;
         }
         token = strtok(buffer, " ");
         for (int j = 0; token != NULL && j < 3; j++) {
-            //printf("%s\n",token);
+            // printf("%s\n",token);
             temp[j] = (uint32_t) strtoul(token, NULL, 10);
             token = strtok(NULL, " ");
         }
-        //printf("%u, %u, %u\n", temp[0], temp[1], temp[2]);
+        // printf("%u, %u, %u\n", temp[0], temp[1], temp[2]);
         graph_add_edge(G, temp[0], temp[1], temp[2]);
     }
+
     fclose(read);
     graph_print(G);
-    //uint32_t x;
-    Path *c = path_create(); //For current path
-    Path *s = path_create(); //For Previous Path
-    FILE *outfile = fopen("outfile.txt", "w");
-    path_push_vertex(c, START_VERTEX,G);
-    //path_pop_vertex(c, &x,G);
-    dfs(G, 0, c, s, outfile, cities);
-    printf("Path Length C: %u\n", path_length(s));
-    printf("path Vertices C: %u\n", path_vertices(s));
 
+    Path *c = path_create(); // For current path
+    Path *s = path_create(); // For Previous Path
+    FILE *outfile = fopen("outfile.txt", "w");
+
+    path_push_vertex(c, 0, G);
+
+    dfs(G, 0, c, s, outfile, cities);
+    printf("Path Length S: %u\n", path_length(s));
+    printf("path Vertices S: %u\n", path_vertices(s));
+    fprintf(outfile, "\n");
+    path_print(s, outfile, cities);
 
     fclose(outfile);
-    //free(cities);
-    //
-    //fclose(outfile);
-
-    graph_delete(&G);
-}
-
-/*
-int main() {
-    Graph *G = graph_create(7, true);
-    graph_add_edge(G, 0, 1, 10);
-    graph_add_edge(G, 1, 2, 2);
-    graph_add_edge(G, 1, 3, 5);
-    graph_add_edge(G, 2, 5, 3);
-    graph_add_edge(G, 2, 6, 5);
-    graph_add_edge(G, 3, 4, 21);
-    graph_print(G);
-    printf("\n");
-
-    path_push_vertex(p, 0, G);
-    printf ("0Path Length: %u\n", path_length(p));  //0
-
-	path_push_vertex(p, 1, G);
-    printf ("1Path Length: %u\n", path_length(p)); //10
-
-    #path_push_vertex(p, 2, G)
-    printf ("2Path Length: %u\n", path_length(p)); //10
-    printf ("Amount of vertices: %u\n", path_vertices(p)); //3
-
-    Path *p = path_create();
-    Path *s = path_create();
-    dfs(G, 0, p, s);
-    char *cities[] = { "El-C", "santee", "lakeside", "chula", "san", "camp", "jam", "lames",
-        "lajol", "tijua" };
-    FILE *outfile = fopen("outfile.txt", "w");
-    path_print(s, outfile, cities);
-    path_delete(&p);
+    path_delete(&c);
     path_delete(&s);
     graph_delete(&G);
 }
-*/
