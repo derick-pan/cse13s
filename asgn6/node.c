1a9bd27c0d177157e23ff5a946a8af848798c76b
Dp <dpan7@ucsc.edu>
2021-05-10 13:11:58 -0700

    Created node.c

--- asgn6/node.c
+++ asgn6/node.c
@@ -2,3 +2,44 @@
 //dpan7
 //node.c
 //
+#include "node.h"
+#include <inttypes.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+typedef struct Node Node;
+//Set node's symbol as symbol, frequency as frequency
+Node *node_create(uint8_t symbol, uint64_t frequency){
+    Node *n = (Node *) malloc(sizeof(Node));
+    n->symbol = symbol;
+    n->frequency = frequency;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void node_delete(Node **n){
+    free(*n);
+    *n = NULL;
+}
+
+//Join left and child node, and return parent
+Node *node_join(Node *left, Node *right){
+    uint8_t name = atoi("$");
+    Node *parentn = node_create(name, (left->frequency + right->frequency));
+    parentn->left = left;
+    parentn->right = right;
+    return parentn;
+}
+
+void node_print(Node *n){
+    if (n){
+        printf("I'm created! \n");
+    }
+    else{
+        printf("i'm not created\n");
+    }
+}
