9a80a2bb5dc7016656b4d9b742c1395fe9734cc4
Dp <dpan7@ucsc.edu>
2021-05-11 21:24:32 -0700

    Enqueue and Dequeue work properly

--- asgn6/node.c
+++ asgn6/node.c
@@ -3,16 +3,16 @@
 //node.c
 //
 #include "node.h"
-#include <inttypes.h>
-#include <stdlib.h>
+
 #include <ctype.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 typedef struct Node Node;
 //Set node's symbol as symbol, frequency as frequency
-Node *node_create(uint8_t symbol, uint64_t frequency){
+Node *node_create(uint8_t symbol, uint64_t frequency) {
     Node *n = (Node *) malloc(sizeof(Node));
     n->symbol = symbol;
     n->frequency = frequency;
@@ -21,13 +21,13 @@ Node *node_create(uint8_t symbol, uint64_t frequency){
     return n;
 }
 
-void node_delete(Node **n){
+void node_delete(Node **n) {
     free(*n);
     *n = NULL;
 }
 
 //Join left and child node, and return parent
-Node *node_join(Node *left, Node *right){
+Node *node_join(Node *left, Node *right) {
     uint8_t name = atoi("$");
     Node *parentn = node_create(name, (left->frequency + right->frequency));
     parentn->left = left;
@@ -35,11 +35,11 @@ Node *node_join(Node *left, Node *right){
     return parentn;
 }
 
-void node_print(Node *n){
-    if (n){
+void node_print(Node *n) {
+    if (n) {
+        printf("my symbol: %u\n", n->symbol);
         printf("I'm created! \n");
-    }
-    else{
+    } else {
         printf("i'm not created\n");
     }
 }
--- asgn6/pq.c
+++ asgn6/pq.c
@@ -2,10 +2,11 @@
 //dpan7
 //pq.c
 #include "pq.h"
+
 #include "node.h"
-#include <inttypes.h>
-#include <stdlib.h>
+
 #include <ctype.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,7 +27,7 @@ typedef struct PriorityQueue {
     Node **items; //Pointer to the first element of array
 } PriorityQueue;
 
-PriorityQueue *pq_create(uint32_t capacity){
+PriorityQueue *pq_create(uint32_t capacity) {
     PriorityQueue *q = (PriorityQueue *) malloc(sizeof(PriorityQueue));
     if (q) {
         q->capacity = capacity;
@@ -34,7 +35,7 @@ PriorityQueue *pq_create(uint32_t capacity){
         q->head = 0;
         q->tail = 0;
         //printf("here");
-        q->items = (Node **) calloc(capacity, sizeof(Node*)); //allocating array of node poitners
+        q->items = (Node **) calloc(capacity, sizeof(Node *)); //allocating array of node poitners
         if (!q->items) {
             free(q);
             q = NULL;
@@ -43,27 +44,27 @@ PriorityQueue *pq_create(uint32_t capacity){
     return q;
 }
 
-void pq_delete(PriorityQueue **q){
+void pq_delete(PriorityQueue **q) {
     free((*q)->items);
     free(*q);
     *q = NULL;
 }
 
-bool pq_empty(PriorityQueue *q){
-    if (q->size == 0 ){
+bool pq_empty(PriorityQueue *q) {
+    if (q->size == 0) {
         return true;
     }
     return false;
 }
 
-bool pq_full(PriorityQueue *q){
+bool pq_full(PriorityQueue *q) {
     if (q->size == q->capacity) {
         return true;
     }
     return false;
 }
 
-uint32_t pq_size(PriorityQueue *q){
+uint32_t pq_size(PriorityQueue *q) {
     return q->size;
 }
 
@@ -75,52 +76,49 @@ uint32_t pq_size(PriorityQueue *q){
 //Mimick an Insertion sort when enqueuing a node, finding
 //a correct position for the node and shifting everything back
 
-bool enqueue(PriorityQueue *q, Node *n){
-    if(pq_full(q)){
+bool enqueue(PriorityQueue *q, Node *n) {
+    if (pq_full(q)) {
         return false;
     }
     // Begin searching for correct position to place node n
-    uint64_t curr = n->frequency;    //The value we need to insert
-    int position = q->tail -1; // Store the position to test
+    uint64_t curr = n->frequency; //The value we need to insert
+    int position = q->tail - 1; // Store the position to test
     //This felt more like a bubble sort
+    //While the testing position is >= 0 and
+    //the Node's frequency is less than the position being tested.
     while (position >= 0 && (curr < ((q->items[position])->frequency))) {
-        q->items[position +1] = q->items[position];
-        position -=1;
+        //We move the node up a spot if so.
+        q->items[position + 1] = q->items[position];
+        position -= 1;
     }
-    q->items[position+1] = n;
-    q->size +=1;
-    q->tail +=1;
-
-return true;
-}
-
-//dequeue node with highest Priority || lowest frequencies
-bool dequeue(PriorityQueue *q, Node **n){
-    if (pq_empty(q)) {
-        return false;
-    }
-    //q->head = next_pos(q->head, q->capacity);
-    *n = q->items[q->head];
-    q->size -=1;
-    *n = q->items[0];
-    //Shift the elements one back
-    for (uint32_t i = 0; i < q->size ; i++){
-        q->items[i] = q->items[i+1];
-    }
-
+    q->items[position + 1] = n;
+    q->size += 1;
+    q->tail += 1;
 
     return true;
 }
 
+//dequeue node with highest Priority || lowest frequencies
+//Pass back the dequeued through the double pointer
+bool dequeue(PriorityQueue *q, Node **n) {
+    if (pq_empty(q)) {
+        return false;
+    }
+    *n = q->items[q->head];
+    q->tail -= 1;
+    q->size -= 1;
+    *n = q->items[0];
+    //Shift the elements one back
+    for (uint32_t i = 0; i < q->size; i++) {
+        q->items[i] = q->items[i + 1];
+    }
+    return true;
+}
 
-void pq_print(PriorityQueue *q){
+void pq_print(PriorityQueue *q) {
 
-    for (uint32_t i = 0 ; i < q->capacity; i++){
+    for (uint32_t i = 0; i < q->size; i++) {
         //printf("%u\n\n", i);
         printf("postion %u  |  symbol: %u\n", i, q->items[i]->symbol);
-
-
-
     }
-
 }
