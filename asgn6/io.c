151f833acbee1c0296695b0438f405978b0fe785
Dp <dpan7@ucsc.edu>
2021-05-14 15:54:25 -0700

    IO's write should be good

--- asgn6/io.c
+++ asgn6/io.c
@@ -21,25 +21,25 @@ static uint8_t buf[BLOCK]; //Declare buffer in io.c
 static int bufind = 0; //Declare buffer index
 
 int read_bytes(int infile, uint8_t *buf, int nbytes) { //Internal function
-    //uint64_t bytes_read = 0; //Total num of bytes read from infile
-    int bytes; //Number of bytes read
-    // -1,0 is the error return , so set it greater than 0
-    //May cause issues
-    while ((bytes = read(infile, buf, nbytes)) > 0 && (int) bytes_read != nbytes) {
+
+    int bytes = 0; //Number of bytes read
+
+    while ((bytes = read(infile, buf, nbytes)) > 0) {
         buf += bytes; //Increase position of buffer
         bytes_read += bytes;
+        nbytes -= bytes;
     }
     return bytes_read;
 }
 
 //Internal Function
 int write_bytes(int outfile, uint8_t *buf, int nbytes) {
-    //int bytes_written = 0; //Total num of bytes written to outfile
-    int bytes; //Number of bytes written in one write
-    // -1,0 is the error return , so set it greater than 0
-    while ((bytes = write(outfile, buf, nbytes)) > 0 && (int) bytes_written != nbytes) {
+
+    int bytes = 0; //Number of bytes written in one write
+    while ((bytes = write(outfile, buf, nbytes)) > 0 /* && (int) bytes_written != nbytes*/) {
         buf += bytes; //Increase position of buffer
         bytes_written += bytes;
+        nbytes -= bytes;
     }
     return bytes_written;
 }
@@ -49,7 +49,7 @@ bool read_bit(int infile, uint8_t *bit) { //Calls read_bytes, used in main
     //Uses functionality of read_bytes
     //if buffer empty or the buffer's index is the size of a block
     //Or if the index is 0, meaning buffer is empty
-    if (bufind == BLOCK || bufind == 0) {
+    if (bufind == BLOCK * 8 || bufind == 0) {
         //Fill the buffer if fillable
         if (read_bytes(infile, buf, BLOCK) == 0) {
             return false;
@@ -66,11 +66,10 @@ bool read_bit(int infile, uint8_t *bit) { //Calls read_bytes, used in main
 void write_code(int outfile, Code *c) { //calls write bytes , used in main
     //Each bit in the code c is buffered into the buffer
     //Create a loop to iterate over the buffer
-
+    //uint8_t temp;
     //Code is based off of Euguene's Pseudocode
     for (uint32_t i = 0; i < c->top; i++) {
-
-        if ((c->bits[i / 8] >> (i % 8) & 0x1) == 1) {
+        if ((c->bits[i / 8] >> (i % 8) & 0x1) == 1) { //If this bit is a 1
             //Set the bit to 1 at buffer
             buf[bufind / 8] |= 0x1 << (bufind % 8);
         } else {
@@ -79,27 +78,33 @@ void write_code(int outfile, Code *c) { //calls write bytes , used in main
         }
         bufind += 1;
 
-        //If the index
-        if (bufind == BLOCK) {
+        //buffind is in bits, block in bytes
+        if (bufind == BLOCK * 8) {
             //Fill the buffer if fillable
+            printf("Should not be in here");
             write_bytes(outfile, buf, BLOCK);
             bufind = 0;
         }
     }
+    //printf("¯_(ツ)_/¯ \n");
 }
 //bufind is the next slot
 void flush_codes(int outfile) { //Write out any leftover buffered bits.
     uint32_t amount = 0;
     if (bufind > 0) {
         //convert num of bits in the buffer to least num of bytes possible
-        if (bufind % 8 > 0) {
-            amount = 1;
-        }
-        amount += bufind;
-        //Zero out some bits
-        for (uint32_t i = bufind; i < amount; i++) { //For loop to zero out the bits
+        //if (bufind % 8 > 0) {
+        //amount = 1;
+        for (int i = bufind; i < BLOCK * 8; i++) { //For loop to zero out the bits
             buf[i / 8] &= ~(0x1 << (i % 8));
         }
-        write_bytes(outfile, buf, amount);
     }
+    printf("bufind %u", bufind);
+    if (bufind % 8 == 0) {
+        amount += bufind / 8;
+    } else {
+        amount += bufind / 8 + 1;
+    }
+
+    write_bytes(outfile, buf, amount);
 }
