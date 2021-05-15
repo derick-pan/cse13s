a4fa89a1a36e96261da19824932b45ec7de0a3c4
Dp <dpan7@ucsc.edu>
2021-05-14 20:01:42 -0700

    Something's wrong with flush codes

--- asgn6/io.c
+++ asgn6/io.c
@@ -21,9 +21,7 @@ static uint8_t buf[BLOCK]; //Declare buffer in io.c
 static int bufind = 0; //Declare buffer index
 
 int read_bytes(int infile, uint8_t *buf, int nbytes) { //Internal function
-
     int bytes = 0; //Number of bytes read
-
     while ((bytes = read(infile, buf, nbytes)) > 0) {
         buf += bytes; //Increase position of buffer
         bytes_read += bytes;
@@ -34,12 +32,12 @@ int read_bytes(int infile, uint8_t *buf, int nbytes) { //Internal function
 
 //Internal Function
 int write_bytes(int outfile, uint8_t *buf, int nbytes) {
-
     int bytes = 0; //Number of bytes written in one write
-    while ((bytes = write(outfile, buf, nbytes)) > 0 /* && (int) bytes_written != nbytes*/) {
+    while (
+        (bytes = write(outfile, buf, nbytes - bytes)) > 0 /* && (int) bytes_written != nbytes*/) {
         buf += bytes; //Increase position of buffer
         bytes_written += bytes;
-        nbytes -= bytes;
+        //nbytes -= bytes;
     }
     return bytes_written;
 }
@@ -49,9 +47,16 @@ bool read_bit(int infile, uint8_t *bit) { //Calls read_bytes, used in main
     //Uses functionality of read_bytes
     //if buffer empty or the buffer's index is the size of a block
     //Or if the index is 0, meaning buffer is empty
+
+    //When buffer empty, fill it
+
+    //if (bufind == (BLOCK*8) -1)
     if (bufind == BLOCK * 8 || bufind == 0) {
+        //if (bufind == (BLOCK * 8)|| bufind == 0) {
         //Fill the buffer if fillable
-        if (read_bytes(infile, buf, BLOCK) == 0) {
+        //printf("yo");
+        //printf("%d",read_bytes(infile, buf, BLOCK));  //Returning 9
+        if (read_bytes(infile, buf, BLOCK) <= 0) {
             return false;
         }
         bufind = 0;
@@ -90,21 +95,23 @@ void write_code(int outfile, Code *c) { //calls write bytes , used in main
 }
 //bufind is the next slot
 void flush_codes(int outfile) { //Write out any leftover buffered bits.
+    printf("%u\n", bufind);
     uint32_t amount = 0;
     if (bufind > 0) {
         //convert num of bits in the buffer to least num of bytes possible
         //if (bufind % 8 > 0) {
         //amount = 1;
-        for (int i = bufind; i < BLOCK * 8; i++) { //For loop to zero out the bits
+        for (int i = bufind; i < (BLOCK * 8); i++) { //For loop to zero out the bits
             buf[i / 8] &= ~(0x1 << (i % 8));
         }
-    }
-    printf("bufind %u", bufind);
-    if (bufind % 8 == 0) {
-        amount += bufind / 8;
-    } else {
-        amount += bufind / 8 + 1;
-    }
 
-    write_bytes(outfile, buf, amount);
+        printf("bufind %u", bufind);
+        if (bufind % 8 == 0) {
+            amount += bufind / 8;
+        } else {
+            amount += bufind / 8 + 1;
+        }
+
+        write_bytes(outfile, buf, amount);
+    }
 }
