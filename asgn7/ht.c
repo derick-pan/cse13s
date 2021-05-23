30131484af70e4c9e0b4ae22c19b227761714be3
Dp <dpan7@ucsc.edu>
2021-05-23 15:09:15 -0700

    Draft of ht.c

---
+++ asgn7/ht.c
@@ -0,0 +1,82 @@
+//Derick Pan
+//dpan7
+//ht.c
+#include "ht.h"
+#include "speck.h"
+
+#include <ctype.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+typedef struct HashTable{
+	uint64_t salt[2];
+	uint32_t size;
+	bool mtf;
+	LinkedList **lists;
+} HashTable;
+
+HashTable *ht_create(uint32_t size, bool mtf) {
+	HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
+	if (ht) {
+		// Leviathan
+		ht->salt[0] = 0x9846e4f157fe8840;
+		ht->salt[1] = 0xc5f318d7e055afb8;
+		ht->size = size;
+		ht->mtf = mtf;
+		ht->lists = (LinkedList **) calloc(size, sizeof(LinkedList *));
+		if (!ht->lists){
+			free(ht);
+			ht = NULL;
+		}
+	}
+	return ht;
+}
+
+void ht_delete(HashTable **ht){
+	if (*ht) {
+        free((*ht)->lists);
+        free(*ht);
+        *ht = NULL;
+    }
+}
+
+uint32_t ht_size(HashTable *ht){
+	return ht->size;
+}
+
+Node *ht_lookup(HashTable *ht, char *oldspeak){
+
+	uint32_t index = hash(ht->salt,oldspeak);
+	return ll_lookup(ht->lists[index], oldspeak);
+}
+
+void ht_insert(HashTable *ht, char *oldspeak, char *newspeak){
+	uint32_t index = hash(ht->salt,oldspeak);
+	//if not initialized then create it
+	if (ht->lists[index] == NULL){
+		ll_create(ht->mtf);
+	}
+	ll_insert(ht->lists[index], oldspeak, newspeak);
+	//ht->size++;
+}
+
+//Returns the num of non-NULL ll in the hash table
+uint32_t ht_count(HashTable *ht){
+	uint32_t count = 0;
+	for (uint32_t i = 0 ; i < ht->size; i++){
+		if (ht->lists[i] != NULL){
+			count++;
+		}
+	}
+	return count;
+}
+
+void ht_print(HashTable *ht){
+	for (uint32_t i = 0 ; i < ht->size; i++){
+		ll_print(ht->lists[i]);
+	}
+	return;
+}
