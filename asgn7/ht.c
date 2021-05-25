d4568266eb3d921f39f515c8abcfa0ea148ee675
Dp <dpan7@ucsc.edu>
2021-05-25 13:26:20 -0700

    ht.c works from prelimary tests

--- asgn7/ht.c
+++ asgn7/ht.c
@@ -1,9 +1,10 @@
 //Derick Pan
 //dpan7
 //ht.c
+#include "ht.h"
+
 #include "bf.h"
 #include "bv.h"
-#include "ht.h"
 #include "ll.h"
 #include "node.h"
 #include "parser.h"
@@ -17,76 +18,76 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef struct HashTable{
-	uint64_t salt[2];
-	uint32_t size;
-	bool mtf;
-	LinkedList **lists;
+typedef struct HashTable {
+    uint64_t salt[2];
+    uint32_t size;
+    bool mtf;
+    LinkedList **lists;
 } HashTable;
 
 HashTable *ht_create(uint32_t size, bool mtf) {
-	HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
-	if (ht) {
-		// Leviathan
-		ht->salt[0] = 0x9846e4f157fe8840;
-		ht->salt[1] = 0xc5f318d7e055afb8;
-		ht->size = size;
-		ht->mtf = mtf;
-		ht->lists = (LinkedList **) calloc(size, sizeof(LinkedList *));
-		if (!ht->lists){
-			free(ht);
-			ht = NULL;
-		}
-	}
-	return ht;
+    HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
+    if (ht) {
+        // Leviathan
+        ht->salt[0] = 0x9846e4f157fe8840;
+        ht->salt[1] = 0xc5f318d7e055afb8;
+        ht->size = size;
+        ht->mtf = mtf;
+        ht->lists = (LinkedList **) calloc(size, sizeof(LinkedList *));
+        if (!ht->lists) {
+            free(ht);
+            ht = NULL;
+        }
+    }
+    return ht;
 }
 
-void ht_delete(HashTable **ht){
-	if (*ht) {
+void ht_delete(HashTable **ht) {
+    if (*ht) {
         free((*ht)->lists);
         free(*ht);
         *ht = NULL;
     }
 }
 
-uint32_t ht_size(HashTable *ht){
-	return ht->size;
+uint32_t ht_size(HashTable *ht) {
+    return ht->size;
 }
 
 //Search for a node that contains oldspeak
-Node *ht_lookup(HashTable *ht, char *oldspeak){
-	uint32_t index = hash(ht->salt,oldspeak)% ht->size;
-	return ll_lookup(ht->lists[index], oldspeak);
+Node *ht_lookup(HashTable *ht, char *oldspeak) {
+    uint32_t index = hash(ht->salt, oldspeak) % ht->size;
+    return ll_lookup(ht->lists[index], oldspeak);
 }
 
-void ht_insert(HashTable *ht, char *oldspeak, char *newspeak){
-	uint32_t index = hash(ht->salt,oldspeak) % ht->size;
-	//if not initialized then create it
-	printf("Index: %u Size: %u\n",index,ht->size);
-	if (!ht->lists[index]){
-		printf("Create ll \n\n");
-		ht->lists[index] = ll_create(ht->mtf);
-	}
-	printf("Crash before insert\n");
-	ll_insert(ht->lists[index], oldspeak, newspeak);
+void ht_insert(HashTable *ht, char *oldspeak, char *newspeak) {
+    uint32_t index = hash(ht->salt, oldspeak) % ht->size;
+    //if not initialized then create it
+    printf("Index: %u Size: %u\n", index, ht->size);
+    if (!ht->lists[index]) {
+        printf("Create ll \n\n");
+        ht->lists[index] = ll_create(ht->mtf);
+    }
+    //printf("Crash before insert\n");
+    ll_insert(ht->lists[index], oldspeak, newspeak);
 }
 
 //Returns the num of non-NULL ll in the hash table
-uint32_t ht_count(HashTable *ht){
-	uint32_t count = 0;
-	for (uint32_t i = 0 ; i < ht->size; i++){
-		if (ht->lists[i] != NULL){
-			count++;
-		}
-	}
-	return count;
+uint32_t ht_count(HashTable *ht) {
+    uint32_t count = 0;
+    for (uint32_t i = 0; i < ht->size; i++) {
+        if (ht->lists[i] != NULL) {
+            count++;
+        }
+    }
+    return count;
 }
 
-void ht_print(HashTable *ht){
-	for (uint32_t i = 0 ; i < ht->size; i++){
-		if(ht->lists[i]){
-			ll_print(ht->lists[i]);
-		}
-	}
-	return;
+void ht_print(HashTable *ht) {
+    for (uint32_t i = 0; i < ht->size; i++) {
+        if (ht->lists[i]) {
+            ll_print(ht->lists[i]);
+        }
+    }
+    return;
 }
